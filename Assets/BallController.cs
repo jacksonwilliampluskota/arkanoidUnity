8d3e899403cc1c69f650e8be5dd8a4f06380f8e7
jackson william pluskota <jacksonw7007@hotmail.com>
2018-03-14 01:30:34 -0300

    ball quick

--- Assets/BallController.cs
+++ Assets/BallController.cs
@@ -6,6 +6,8 @@ public class BallController : MonoBehaviour {
 	public Vector3 StartDirection = new Vector3(0f,2f,0f);
 	private Rigidbody _rb;
 
+	private bool _collided;
+
 	void Awake()
 	{
 		_rb = GetComponent<Rigidbody>();
@@ -13,10 +15,23 @@ public class BallController : MonoBehaviour {
 
 	void Update()
 	{
+
 		if (Input.GetButton("Jump")  && Game.Manager.IsDead)
 		{
 			ApplyStartForce();
 		}
+	}
+
+	void FixedUpdate()
+	{
+		if (!_collided)
+			return;
+		//var ySpeedDiff = StartDirection.y - _rb.velocity.y;
+		//_rb.velocity = new Vector3(0,0,0);
+		var newY = (_rb.velocity.y > 0f) ? StartDirection.y : -StartDirection.y;
+		_rb.AddForce(new Vector3(0, newY * 0.01f, 0f), ForceMode.VelocityChange);
+		//_rb.velocity = new Vector3(_rb.velocity.x, newY * 0.01f, 0f);
+		_collided = false;
 	}
 
 	void ApplyStartForce()
@@ -27,6 +42,7 @@ public class BallController : MonoBehaviour {
 
 	private void OnCollisionEnter(Collision collision)
 	{
+		_collided = true;
 		if (collision.gameObject == Game.Manager.PlayerPaddle.gameObject)
 			return;
 		var cell = collision.gameObject.GetComponent<Cell>();
@@ -34,6 +50,5 @@ public class BallController : MonoBehaviour {
 		if (cell == null)
 			return;
 		cell.WasHit();
-		
 	}
 }
