77a76b35a0cd70164a3c522b5dc33c3ba86ad65a
jackson william pluskota <jacksonw7007@hotmail.com>
2018-03-16 01:52:31 -0300

    score count

--- Assets/Game.cs
+++ Assets/Game.cs
@@ -1,14 +1,28 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Game : MonoBehaviour {
 
 	public static Game Manager { get; private set;  }
 	public int Lives;
-	public int Score;
 	public Paddle PlayerPaddle;
 	public bool IsDead = true;
+
+	private int _score;
+	public int Score {
+		get { return _score;  }
+
+		set
+		{
+			_score = value;
+			if (ScoreTxt != null)
+				ScoreTxt.text = "Score: " + Score;
+		}
+	}
+
+	public Text ScoreTxt;
 
 	//public bool IsDead {
 	//	get {
